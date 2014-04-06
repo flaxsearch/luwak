4d7deef129fb7bf216c8fa1d931578f3849eeea6
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2014-04-06 10:10:50 +0100

    Fix TermsFilterTermExtractor logic for multiple terms/fields, add test
    Closes #4

--- src/test/java/uk/co/flax/luwak/TermsFilterTermExtractorTest.java
+++ src/test/java/uk/co/flax/luwak/TermsFilterTermExtractorTest.java
@@ -1,7 +1,5 @@
 package uk.co.flax.luwak;
 
-import java.util.LinkedList;
-import java.util.List;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queries.TermsFilter;
 import org.apache.lucene.search.Filter;
@@ -10,6 +8,11 @@ import org.junit.Before;
 import org.junit.Test;
 import uk.co.flax.luwak.termextractor.QueryTerm;
 import uk.co.flax.luwak.termextractor.TermsFilterTermExtractor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class TermsFilterTermExtractorTest {
 
@@ -21,13 +24,27 @@ public class TermsFilterTermExtractorTest {
     }
 
     @Test
-    public void testExtract() {
+    public void testExtractSingleTerm() {
         List<QueryTerm> terms = new LinkedList<>();
         Filter filter = new TermsFilter(new Term("someField", "123"));
         extractor.extract(filter, terms);
         Assert.assertEquals(1, terms.size());
         Assert.assertEquals("someField", terms.get(0).field);
         Assert.assertEquals("123", terms.get(0).term);
+    }
+
+    @Test
+    public void testExtractMultipleTerms() {
+
+        Filter filter = new TermsFilter(new Term("field1", "foo"), new Term("field2", "bar"), new Term("field1", "baz"));
+
+        List<QueryTerm> terms = new LinkedList<>();
+        extractor.extract(filter, terms);
+
+        assertThat(terms).hasSize(3);
+        assertThat(terms).contains(new QueryTerm("field1", "foo", QueryTerm.Type.EXACT));
+        assertThat(terms).contains(new QueryTerm("field2", "bar", QueryTerm.Type.EXACT));
+        assertThat(terms).contains(new QueryTerm("field1", "baz", QueryTerm.Type.EXACT));
     }
 
 }
