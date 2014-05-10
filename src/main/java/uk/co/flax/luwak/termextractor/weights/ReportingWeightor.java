5550d27728f506304e8e68220b067ab6ad753b1f
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2014-05-10 16:46:28 +0100

    Add a ReportingWeightor to help with debugging

---
+++ src/main/java/uk/co/flax/luwak/termextractor/weights/ReportingWeightor.java
@@ -0,0 +1,42 @@
+package uk.co.flax.luwak.termextractor.weights;
+
+import uk.co.flax.luwak.termextractor.QueryTerm;
+
+import java.util.List;
+
+/**
+ * Copyright (c) 2014 Lemur Consulting Ltd.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class ReportingWeightor implements TermWeightor {
+
+    private final TermWeightor delegate;
+    private final Reporter reporter;
+
+    public ReportingWeightor(Reporter reporter, TermWeightor delegate) {
+        this.delegate = delegate;
+        this.reporter = reporter;
+    }
+
+    @Override
+    public float weigh(List<QueryTerm> terms) {
+        float weight = delegate.weigh(terms);
+        reporter.report(weight, terms);
+        return weight;
+    }
+
+    public static interface Reporter {
+        void report(float weight, List<QueryTerm> terms);
+    }
+}
