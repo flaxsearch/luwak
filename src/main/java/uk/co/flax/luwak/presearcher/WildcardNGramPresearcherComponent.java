03350a8785bdeb9b367648824e3beb74dd113322
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2014-10-17 12:48:30 +0100

    Add OrderedTokenFilter to WildcardNGramPresearcherComponent analysis chain
    Document build times are around 40 times higher with this presearcher component
    than with the default setup, which is probably due to lots of disk seeks in the
    TermsEnumFilter.  OrderedTokenFilter ensures that the token stream being passed
    to TermsEnumFilter is in order, which should speed things up.

--- src/main/java/uk/co/flax/luwak/presearcher/WildcardNGramPresearcherComponent.java
+++ src/main/java/uk/co/flax/luwak/presearcher/WildcardNGramPresearcherComponent.java
@@ -3,6 +3,7 @@ package uk.co.flax.luwak.presearcher;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.miscellaneous.KeywordRepeatFilter;
 import uk.co.flax.luwak.analysis.DuplicateRemovalTokenFilter;
+import uk.co.flax.luwak.analysis.OrderedTokenFilter;
 import uk.co.flax.luwak.analysis.SuffixingNGramTokenFilter;
 import uk.co.flax.luwak.termextractor.QueryTerm;
 import uk.co.flax.luwak.termextractor.treebuilder.RegexpNGramTermQueryTreeBuilder;
@@ -75,7 +76,7 @@ public class WildcardNGramPresearcherComponent extends PresearcherComponent {
         TokenStream duped = new KeywordRepeatFilter(ts);
         TokenStream ngrammed
                 = new SuffixingNGramTokenFilter(duped, ngramSuffix, wildcardToken, maxTokenSize);
-        return new DuplicateRemovalTokenFilter(ngrammed);
+        return new OrderedTokenFilter(new DuplicateRemovalTokenFilter(ngrammed));
     }
 
     @Override
