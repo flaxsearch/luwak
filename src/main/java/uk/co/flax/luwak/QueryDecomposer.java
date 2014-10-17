5d75003e4e34a1df1901a12406693e56f43861c0
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2014-10-17 09:05:46 +0100

    Allow QueryDecomposer to decomposer disjunctions with exclusions
    So, eg, (this that other -take) can be decomposed to (this -take), (that -take),
    (other -take).

--- src/main/java/uk/co/flax/luwak/QueryDecomposer.java
+++ src/main/java/uk/co/flax/luwak/QueryDecomposer.java
@@ -1,5 +1,6 @@
 package uk.co.flax.luwak;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -28,26 +29,42 @@ import org.apache.lucene.search.Query;
 public class QueryDecomposer {
 
     public Collection<Query> decompose(Query q) {
-        if (!canBeDecomposed(q))
-            return ImmutableList.of(q);
 
-        List<Query> subqueries = new LinkedList<>();
-        for (BooleanClause clause : (BooleanQuery) q) {
-            subqueries.addAll(decompose(clause.getQuery()));
-        }
-        return subqueries;
+        if (q instanceof BooleanQuery)
+            return decomposeBoolean((BooleanQuery) q);
+
+        return ImmutableList.of(q);
     }
 
-    public static boolean canBeDecomposed(Query query) {
-        if (!(query instanceof BooleanQuery))
-            return false;
-        BooleanQuery bq = (BooleanQuery) query;
-        if (bq.getMinimumNumberShouldMatch() > 1)
-            return false;
-        for (BooleanClause clause : bq) {
-            if (clause.getOccur() != BooleanClause.Occur.SHOULD)
-                return false;
+    public Collection<Query> decomposeBoolean(BooleanQuery q) {
+        if (q.getMinimumNumberShouldMatch() > 1)
+            return ImmutableList.<Query>of(q);
+
+        List<Query> subqueries = new LinkedList<>();
+        List<Query> exclusions = new LinkedList<>();
+
+        for (BooleanClause clause : q) {
+            // Any MUST clauses mean we can't decompose
+            if (clause.getOccur() == BooleanClause.Occur.MUST)
+                return ImmutableList.<Query>of(q);
+            if (clause.getOccur() == BooleanClause.Occur.MUST_NOT)
+                exclusions.add(clause.getQuery());
+            else
+                subqueries.addAll(decompose(clause.getQuery()));
         }
-        return true;
+
+        if (exclusions.size() == 0)
+            return subqueries;
+
+        List<Query> rewrittenSubqueries = new ArrayList<>(subqueries.size());
+        for (Query subquery : subqueries) {
+            BooleanQuery bq = new BooleanQuery();
+            bq.add(subquery, BooleanClause.Occur.MUST);
+            for (Query ex : exclusions) {
+                bq.add(ex, BooleanClause.Occur.MUST_NOT);
+            }
+            rewrittenSubqueries.add(bq);
+        }
+        return rewrittenSubqueries;
     }
 }
