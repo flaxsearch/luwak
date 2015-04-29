6f84d54c420dcc47ae25d73e78c978eda68a8ba6
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2015-04-29 17:59:02 +0100

    Add BenchmarkResults.toString()

--- benchmark/src/main/java/uk/co/flax/luwak/benchmark/BenchmarkResults.java
+++ benchmark/src/main/java/uk/co/flax/luwak/benchmark/BenchmarkResults.java
@@ -15,8 +15,12 @@ package uk.co.flax.luwak.benchmark;
  *   limitations under the License.
  */
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
+import com.codahale.metrics.Snapshot;
 import com.codahale.metrics.Timer;
 import uk.co.flax.luwak.Matches;
 import uk.co.flax.luwak.QueryMatch;
@@ -31,5 +35,33 @@ public class BenchmarkResults<T extends QueryMatch> {
 
     public Timer getTimer() {
         return timer;
+    }
+
+    @Override
+    public String toString() {
+        StringWriter sw = new StringWriter();
+        Locale locale = Locale.ROOT;
+        try (PrintWriter output = new PrintWriter(sw)) {
+            final Snapshot snapshot = timer.getSnapshot();
+            output.printf(locale, "Benchmark Results%n");
+            output.printf(locale, "             count = %d%n", timer.getCount());
+            output.printf(locale, "         mean rate = %2.2f calls/%s%n", timer.getMeanRate(), "s");
+            output.printf(locale, "     1-minute rate = %2.2f calls/%s%n", timer.getOneMinuteRate(), "s");
+            output.printf(locale, "     5-minute rate = %2.2f calls/%s%n", timer.getFiveMinuteRate(), "s");
+            output.printf(locale, "    15-minute rate = %2.2f calls/%s%n", timer.getFifteenMinuteRate(), "s");
+
+            output.printf(locale, "               min = %d %s%n", TimeUnit.NANOSECONDS.toMillis(snapshot.getMin()), "ms");
+            output.printf(locale, "               max = %d %s%n", TimeUnit.NANOSECONDS.toMillis(snapshot.getMax()), "ms");
+            output.printf(locale, "              mean = %2.2f %s%n", snapshot.getMean() / 1000000, "ms");
+            output.printf(locale, "            stddev = %2.2f %s%n", snapshot.getStdDev() / 1000000, "ms");
+            output.printf(locale, "            median = %2.2f %s%n", snapshot.getMedian() / 1000000, "ms");
+            output.printf(locale, "              75%% <= %2.2f %s%n", snapshot.get75thPercentile() / 1000000, "ms");
+            output.printf(locale, "              95%% <= %2.2f %s%n", snapshot.get95thPercentile() / 1000000, "ms");
+            output.printf(locale, "              98%% <= %2.2f %s%n", snapshot.get98thPercentile() / 1000000, "ms");
+            output.printf(locale, "              99%% <= %2.2f %s%n", snapshot.get99thPercentile() / 1000000, "ms");
+            output.printf(locale, "            99.9%% <= %2.2f %s%n", snapshot.get999thPercentile() / 1000000, "ms");
+
+        }
+        return sw.toString();
     }
 }
