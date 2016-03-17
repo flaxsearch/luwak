d7e584139d2933c0d2376986df750354c4ca846d
George Kankava <george.kankava@devfactory.com>
2016-03-17 12:00:56 +0400

    multiple code improvements: squid:S00122, squid:UselessParenthesesCheck

--- luwak/src/main/java/uk/co/flax/luwak/termextractor/QueryTerm.java
+++ luwak/src/main/java/uk/co/flax/luwak/termextractor/QueryTerm.java
@@ -79,21 +79,31 @@ public class QueryTerm {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         QueryTerm queryTerm = (QueryTerm) o;
 
-        if (term != null ? !term.equals(queryTerm.term) : queryTerm.term != null) return false;
-        if (type != null ? !type.equals(queryTerm.type) : queryTerm.type != null) return false;
-        if (payload != null ? !payload.equals(queryTerm.payload) : queryTerm.payload != null) return false;
+        if (term != null ? !term.equals(queryTerm.term) : queryTerm.term != null) {
+            return false;
+        }
+        if (type != null ? !type.equals(queryTerm.type) : queryTerm.type != null) {
+            return false;
+        }
+        if (payload != null ? !payload.equals(queryTerm.payload) : queryTerm.payload != null) {
+            return false;
+        }
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        int result = (term != null ? term.hashCode() : 0);
+        int result = term != null ? term.hashCode() : 0;
         result = 31 * result + (type != null ? type.hashCode() : 0);
         result = 31 * result + (payload != null ? payload.hashCode() : 0);
         return result;
