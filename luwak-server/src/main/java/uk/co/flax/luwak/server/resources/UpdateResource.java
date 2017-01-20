662435bca6fc9ccf748eb65f976cfe45a3a516c0
Michael Kearns <michael.kearns@softwire.com>
2017-01-20 14:08:59 +0000

    Use monitor in resources
    This means the service update / match resources now work.
    Huzzah.

--- luwak-server/src/main/java/uk/co/flax/luwak/server/resources/UpdateResource.java
+++ luwak-server/src/main/java/uk/co/flax/luwak/server/resources/UpdateResource.java
@@ -1,11 +1,14 @@
 package uk.co.flax.luwak.server.resources;
 
 import uk.co.flax.luwak.Monitor;
+import uk.co.flax.luwak.MonitorQuery;
+import uk.co.flax.luwak.UpdateException;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 /**
  * Created by Tom.Ridd on 20/01/2017.
@@ -21,8 +24,8 @@ public class UpdateResource {
   @POST
   @Path("/")
   @Produces(MediaType.APPLICATION_JSON)
-  public LuwakUpdateResult postLuwakUpdate(String inputDocumentJson) {
-
-    return new LuwakUpdateResult(inputDocumentJson, true);
+  public LuwakUpdateResult postLuwakUpdate(MonitorQuery monitorQuery) throws IOException, UpdateException {
+    monitor.update(monitorQuery);
+    return new LuwakUpdateResult(monitorQuery, true);
   }
 }
