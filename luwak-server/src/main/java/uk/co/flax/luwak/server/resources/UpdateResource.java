e8c1223e7b2c7009d912e172855a70799d554c1a
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2017-01-20 14:59:04 +0000

    Add multiple query update endpoint

--- luwak-server/src/main/java/uk/co/flax/luwak/server/resources/UpdateResource.java
+++ luwak-server/src/main/java/uk/co/flax/luwak/server/resources/UpdateResource.java
@@ -1,31 +1,39 @@
 package uk.co.flax.luwak.server.resources;
 
-import uk.co.flax.luwak.Monitor;
-import uk.co.flax.luwak.MonitorQuery;
-import uk.co.flax.luwak.UpdateException;
-
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.util.List;
+
+import uk.co.flax.luwak.Monitor;
+import uk.co.flax.luwak.MonitorQuery;
+import uk.co.flax.luwak.UpdateException;
 
 /**
  * Created by Tom.Ridd on 20/01/2017.
  */
 @Path("/update")
 public class UpdateResource {
-  private final Monitor monitor;
+    private final Monitor monitor;
 
-  public UpdateResource(Monitor monitor) {
-    this.monitor = monitor;
-  }
+    public UpdateResource(Monitor monitor) {
+        this.monitor = monitor;
+    }
 
-  @POST
-  @Path("/")
-  @Produces(MediaType.APPLICATION_JSON)
-  public LuwakUpdateResult postLuwakUpdate(MonitorQuery monitorQuery) throws IOException, UpdateException {
-    monitor.update(monitorQuery);
-    return new LuwakUpdateResult(monitorQuery, true);
-  }
+    @POST
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public LuwakUpdateResult postLuwakUpdate(MonitorQuery monitorQuery) throws IOException, UpdateException {
+        monitor.update(monitorQuery);
+        return new LuwakUpdateResult(monitorQuery, true);
+    }
+
+    @POST
+    @Path("/multi")
+    public LuwakUpdateResult addMultiple(List<MonitorQuery> queries) throws Exception {
+        monitor.update(queries);
+        return new LuwakUpdateResult(null, true);
+    }
 }
