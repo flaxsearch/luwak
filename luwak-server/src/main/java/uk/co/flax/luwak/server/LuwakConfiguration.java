9f58487bfabc2e4be701fcc915e3b97aa6bb7d64
okilian <jabamaya@yahoo.de>
2017-01-20 11:43:18 +0000

    add the stubs to the Configuration

--- luwak-server/src/main/java/uk/co/flax/luwak/server/LuwakConfiguration.java
+++ luwak-server/src/main/java/uk/co/flax/luwak/server/LuwakConfiguration.java
@@ -15,7 +15,35 @@ package uk.co.flax.luwak.server;
  *   limitations under the License.
  */
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class LuwakConfiguration extends Configuration {
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "LuwakServer";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
 }
